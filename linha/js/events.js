5d943bdf8825e1dd364423fbb6c24e6addce6166
unknown <kcireh.i@gmail.com>
2023-02-16 12:05:08 -0300

    ok

--- linha/js/events.js
+++ linha/js/events.js
@@ -17,16 +17,16 @@ bt_half.onclick = () => {
     values.setBet(values.bet / 2)
 }
 
-bt_plus.onclick = () => {
-    if(!button.checkName('Apostar')) return
-    values.setMult(parseFloat((values.mult*=1.3).toFixed(2)))
-}
+// bt_plus.onclick = () => {
+//     if(!button.checkName('Apostar')) return
+//     values.setMult(parseFloat((values.mult*=1.3).toFixed(2)))
+// }
 
-bt_less.onclick = () => {
-    if(!button.checkName('Apostar')) return
-    if(values.mult <= 1) return
-    values.setMult(parseFloat((values.mult/=1.3).toFixed(2)))
-}
+// bt_less.onclick = () => {
+//     if(!button.checkName('Apostar')) return
+//     if(values.mult <= 1) return
+//     values.setMult(parseFloat((values.mult/=1.3).toFixed(2)))
+// }
 
 bt_main.onclick = () => logic.collect()
 
